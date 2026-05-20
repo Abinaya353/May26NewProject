b9de4ad38dfa6c92dec3a5a3d9ab1d18a6d1d036
Abinaya353 <greensrxv0907@gmail.com>
2026-05-20 18:48:15 +0530

    Done

---
+++ Validation/Sum.java
@@ -0,0 +1,23 @@
+package org.Validation;
+
+public class Sum {
+	
+	public static void main(String[] args) {
+		
+		int a = 10;
+		 
+		int b = 20;
+		
+		int c = a= b ;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
